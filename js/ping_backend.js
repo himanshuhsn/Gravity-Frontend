941eea8c51b08cc1445be773768e475686040af7
Himanshu <49199845+himanshuhsn@users.noreply.github.com>
2023-03-06 20:40:29 +0530

    feat: ping backend to keep awake (#6)
    * feat: ping backend every 10 min to keep awake.

---
+++ js/ping_backend.js
@@ -0,0 +1,15 @@
+let INTERVAL = 10 * 60 * 1000; // 10 min
+let PING_URL = url + '/api/game/';
+
+function checkData() {
+    $.ajax({
+        type: "GET",
+        url: PING_URL,
+        success: function (data) {
+            // console.log("pinged the backend server")
+            // console.log(data);
+        }
+    });
+}
+
+setInterval(checkData, INTERVAL);
